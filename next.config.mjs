828c0b2c7812a471b0a7a91fba715b50b4f06c76
Leon Wright <c-lwright@denaliai.com>
2024-05-16 16:56:18 -0500

    removed the extra shit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
-
-const nextConfig = withPWA({
-    pwa: {
-        dest: 'public',
-        register: true,
-        skipWaiting: true,
-    },
-});
+const nextConfig = {};
 
 export default nextConfig;
